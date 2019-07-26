5131e6e80b25be661e65bd754e74962ccc692a42
keneanung <keneanung@googlemail.com>
2019-07-26 13:14:44 +0200

    Use StartProcess to wait for the installer to finish

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ cd C:\src
 $url = "http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe"
 $file = "C:\src\qt-installer.exe"
 (New-Object System.Net.WebClient).DownloadFile($url, $file)
-.\qt-installer --script=qt-silent-install.qs
+Start-Process -FilePath $file -Wait -PassThru -NoNewWindow -Argument-List @("--script=C:\src\qt-silent-install.qs")
 Remove-Item qt-installer.exe
 Remove-Item qt-silent-install.qs
 Remove-Item install.ps1
