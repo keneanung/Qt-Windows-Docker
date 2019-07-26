134a9eb833a3956db4d7ea74f02739cbac132206
keneanung <keneanung@googlemail.com>
2019-07-26 13:10:05 +0200

    Be explicit about qhere to save the installer

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ FROM mcr.microsoft.com/windows/servercore:ltsc2019
 COPY qt-silent-install.qs C:\src\
 COPY install.ps1 C:\src\
 
-RUN dir C:\src
 RUN powershell -File C:\src\install.ps1
--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $global:ErrorActionPreference = "Stop"
 cd C:\src
 $url = "http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe"
-$file = "qt-installer.exe"
+$file = "C:\src\qt-installer.exe"
 (New-Object System.Net.WebClient).DownloadFile($url, $file)
 .\qt-installer --script=qt-silent-install.qs
 Remove-Item qt-installer.exe
